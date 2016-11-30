7ac68a8629db70770f0c1086bb656c62725d05d8
Daiderd Jordan <daiderd@gmail.com>
2016-11-30 23:12:06 +0100

    launchd: properly generate plist files

--- modules/launchd/lib.nix
+++ modules/launchd/lib.nix
@@ -2,27 +2,56 @@
 
 with lib;
 
+let
+
+  attrFilter = name: value: name != "_module" && value != null;
+
+in
+
 rec {
 
-  xmlMap = f: xs: ''
-    <array>
-    ${concatMapStringsSep "\n" f xs}
-    </array>
-  '';
+  toPLIST = x: ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+  '' + pprExpr "" x
+     + "\n</plist>";
 
-  xmlMapAttrs = f: attr: ''
-    <dict>
-    ${concatStringsSep "\n" (mapAttrsFlatten (xmlMapAttr f) attr)}
-    </dict>
-  '';
+  pprExpr = ind: x:
+    if isNull x then "" else
+    if isBool x then pprBool ind x else
+    if isInt x then pprInt ind x else
+    if isString x then pprStr ind x else
+    if isList x then pprList ind x else
+    if isAttrs x then pprAttrs ind x else
+    throw "invalid plist type";
 
-  xmlMapAttr = f: n: v: optionalString (v != null) ''
-    <key>${n}</key>
-    ${f v}
-  '';
+  pprLiteral = ind: x: ind + x;
 
-  xmlBool = x: if x then "<true/>" else "<false/>";
-  xmlInt = x: "<integer>${toString x}</integer>";
-  xmlString = x: "<string>${x}</string>";
+  pprBool = ind: x: pprLiteral ind  (if x then "<true/>" else "<false/>");
+  pprInt = ind: x: pprLiteral ind "<integer>${toString x}</integer>";
+  pprStr = ind: x: pprLiteral ind "<string>${x}</string>";
+  pprKey = ind: x: pprLiteral ind "<key>${x}</key>";
+
+  pprIndent = ind: (pprExpr "\t${ind}");
+
+  pprItem = ind: concatMapStringsSep "\n" (pprIndent ind);
+
+  pprList = ind: x: concatStringsSep "\n" [
+    (pprLiteral ind "<array>")
+    (pprItem ind x)
+    (pprLiteral ind "</array>")
+  ];
+
+  pprAttrs = ind: x: concatStringsSep "\n" [
+    (pprLiteral ind "<dict>")
+    (pprAttr ind x)
+    (pprLiteral ind "</dict>")
+  ];
+
+  pprAttr = ind: x: concatStringsSep "\n" (flatten (mapAttrsToList (name: value: optional (attrFilter name value) [
+    (pprKey "\t${ind}" name)
+    (pprExpr "\t${ind}" value)
+  ]) x));
 
 }
