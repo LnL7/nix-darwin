0c9710c375adc9c098f23a4a6d88f5c4292a75a0
Daiderd Jordan <daiderd@gmail.com>
2017-01-29 21:26:23 +0100

    lnl: add fzf history widget

--- modules/examples/lnl.nix
+++ modules/examples/lnl.nix
@@ -177,6 +177,42 @@
 
   programs.zsh.interactiveShellInit = ''
     setopt AUTOCD AUTOPUSHD
+
+    autoload -U down-line-or-beginning-search
+    autoload -U up-line-or-beginning-search
+    bindkey '^[[A' down-line-or-beginning-search
+    bindkey '^[[A' up-line-or-beginning-search
+    zle -N down-line-or-beginning-search
+    zle -N up-line-or-beginning-search
+
+    __fzf_use_tmux__() {
+      [ -n "$TMUX_PANE" ] && [ "''${FZF_TMUX:-0}" != 0 ] && [ ''${LINES:-40} -gt 15 ]
+    }
+
+    __fzfcmd() {
+      __fzf_use_tmux__ &&
+        echo "fzf-tmux -d''${FZF_TMUX_HEIGHT:-40%}" || echo "fzf"
+    }
+
+    # CTRL-R - Paste the selected command from history into the command line
+    fzf-history-widget() {
+      local selected num
+      setopt localoptions noglobsubst pipefail 2> /dev/null
+      selected=( $(fc -l 1 |
+        FZF_DEFAULT_OPTS="--height ''${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS +s --tac -n2..,.. --tiebreak=index --toggle-sort=ctrl-r $FZF_CTRL_R_OPTS --query=''${(q)LBUFFER} +m" $(__fzfcmd)) )
+      local ret=$?
+      if [ -n "$selected" ]; then
+        num=$selected[1]
+        if [ -n "$num" ]; then
+          zle vi-fetch-history -n $num
+        fi
+      fi
+      zle redisplay
+      typeset -f zle-line-init >/dev/null && zle zle-line-init
+      return $ret
+    }
+    zle     -N   fzf-history-widget
+    bindkey '^R' fzf-history-widget
   '';
 
   environment.variables.HOMEBREW_CASK_OPTS = "--appdir=/Applications/cask";
