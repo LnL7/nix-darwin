b09fdc4f2f757923d925416c9b4cf3baf68a03be
Daiderd Jordan <daiderd@gmail.com>
2017-07-23 13:38:27 +0200

    bootstrap: add channel as root

--- bootstrap.sh
+++ bootstrap.sh
@@ -160,11 +160,11 @@ install(){
 
     # Fetch the nix-darwin repo
     echo -e ""$YELLOW"Configuring darwin channel..."$ESC""
-    nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin || exit
-    nix-channel --update || exit
+    sudo nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin || exit
+    sudo nix-channel --update || exit
 
     # Create symlink for old NIX_PATH entry
-    ln -sfn "/nix/var/nix/profiles/per-user/$USER/channels/darwin" "$HOME/.nix-defexpr/darwin" || exit
+    ln -sfn "/nix/var/nix/profiles/per-user/root/channels/darwin" "$HOME/.nix-defexpr/darwin" || exit
 
     # Copy the example configuration
     echo -e "Copying example configuration to "$YELLOW"~/.nixpkgs/darwin-configuration.nix"$ESC"..."
