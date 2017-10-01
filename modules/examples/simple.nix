c8cc548dfc53cd2f100d64cab3c50e86486e2fd0
Daiderd Jordan <daiderd@gmail.com>
2017-10-01 16:18:37 +0200

    examples-simple: add stateVersion

--- modules/examples/simple.nix
+++ modules/examples/simple.nix
@@ -13,6 +13,10 @@
   # Recreate /run/current-system symlink after boot.
   services.activate-system.enable = true;
 
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 2;
+
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
   nix.maxJobs = 1;
