1dd1f60f90036af82dfced50b85478b713ab7432
Colin Barrett <colin@springsandstruts.com>
2017-10-11 20:53:57 -0400

    nixos -> nix_darwin

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -119,7 +119,7 @@ in
       # /etc/fish/config.fish: DO NOT EDIT -- this file has been generated automatically.
 
       # if we haven't sourced the general config, do it
-      if not set -q __fish_nixos_general_config_sourced
+      if not set -q __fish_nix_darwin_general_config_sourced
         set fish_function_path ${pkgs.fish-foreign-env}/share/fish-foreign-env/functions $fish_function_path
         fenv source /etc/fish/foreign-env/shellInit > /dev/null
         set -e fish_function_path[1]
@@ -128,11 +128,11 @@ in
 
         # and leave a note so we don't source this config section again from
         # this very shell (children will source the general config anew)
-        set -g __fish_nixos_general_config_sourced 1
+        set -g __fish_nix_darwin_general_config_sourced 1
       end
 
       # if we haven't sourced the login config, do it
-      status --is-login; and not set -q __fish_nixos_login_config_sourced
+      status --is-login; and not set -q __fish_nix_darwin_login_config_sourced
       and begin
         set fish_function_path ${pkgs.fish-foreign-env}/share/fish-foreign-env/functions $fish_function_path
         fenv source /etc/fish/foreign-env/loginShellInit > /dev/null
@@ -142,11 +142,11 @@ in
 
         # and leave a note so we don't source this config section again from
         # this very shell (children will source the general config anew)
-        set -g __fish_nixos_login_config_sourced 1
+        set -g __fish_nix_darwin_login_config_sourced 1
       end
 
       # if we haven't sourced the interactive config, do it
-      status --is-interactive; and not set -q __fish_nixos_interactive_config_sourced
+      status --is-interactive; and not set -q __fish_nix_darwin_interactive_config_sourced
       and begin
         ${fishAliases}
         
@@ -161,7 +161,7 @@ in
         # and leave a note so we don't source this config section again from
         # this very shell (children will source the general config anew,
         # allowing configuration changes in, e.g, aliases, to propagate)
-        set -g __fish_nixos_interactive_config_sourced 1
+        set -g __fish_nix_darwin_interactive_config_sourced 1
       end
     '';
 
