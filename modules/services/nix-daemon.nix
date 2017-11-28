48af45075acb0e837582181f5e6522c1a657f060
Daiderd Jordan <daiderd@gmail.com>
2017-11-28 22:25:51 +0100

    nix-daemon: add option to configure a logfile

--- modules/services/nix-daemon.nix
+++ modules/services/nix-daemon.nix
@@ -14,6 +14,18 @@ in
       description = "Whether to activate system at boot time.";
     };
 
+    services.nix-daemon.logFile = mkOption {
+      type = types.nullOr types.path;
+      default = null;
+      example = "/var/log/nix-daemon.log";
+      description = ''
+        The logfile to use for the nix-daemon service. Alternatively
+        <command>sudo launchctl debug system/org.nixos.nix-daemon --stderr</command>
+        can be used to stream the logs to a shell after restarting the service with
+        <command>sudo launchctl kickstart -k system/org.nixos.nix-daemon</command>.
+      '';
+    };
+
     services.nix-daemon.tempDir = mkOption {
       type = types.nullOr types.path;
       default = null;
@@ -38,6 +50,7 @@ in
       serviceConfig.LowPriorityIO = config.nix.daemonIONice;
       serviceConfig.Nice = config.nix.daemonNiceLevel;
       serviceConfig.SoftResourceLimits.NumberOfFiles = 4096;
+      serviceConfig.StandardErrorPath = cfg.logFile;
 
       serviceConfig.EnvironmentVariables = config.nix.envVars
         // { NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"; }
