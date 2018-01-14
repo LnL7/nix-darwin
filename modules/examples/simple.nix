d6e85130f4a0a14b2bfa20377a4a8cfa791d28ba
Daiderd Jordan <daiderd@gmail.com>
2018-01-14 19:31:48 +0100

    examples: add zsh/fish options

--- modules/examples/simple.nix
+++ modules/examples/simple.nix
@@ -9,6 +9,8 @@
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.bash.enable = true;
+  # programs.zsh.enable = true;
+  # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
