d31dbefca77ad42b6b3cc8ce5379ec14717f72a9
Daiderd Jordan <daiderd@gmail.com>
2018-01-14 17:18:10 +0100

    installer: configure build users

--- pkgs/darwin-installer/configuration.nix
+++ pkgs/darwin-installer/configuration.nix
@@ -5,6 +5,11 @@ with lib;
 {
   imports = [ <user-darwin-config> ];
 
+  # NOTE: don't set this outside of the instaler.
+  users.nix.configureBuildUsers = true;
+  users.knownGroups = [ "nixbld" ];
+  users.knownUsers = [ "nixbld1" "nixbld2" "nixbld3" "nixbld4" "nixbld5" "nixbld6" "nixbld7" "nixbld8" "nixbld9" "nixbld10" ];
+
   system.activationScripts.preUserActivation.text = mkBefore ''
     darwinPath=$(NIX_PATH=${concatStringsSep ":" config.nix.nixPath} nix-instantiate --eval -E '<darwin>' 2> /dev/null) || true
     if ! test -e "$darwinPath"; then
