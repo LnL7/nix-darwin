LnL7/nix-darwin

commit 9792b08fdf049524742143f4f788e3eb61a769fd

nix: allways set NIX_REMOTE=daemon when the db isn't writable

modules/nix/default.nix
@@ -356,7 +356,7 @@ in
         NIX_CURRENT_LOAD = "/run/nix/current-load";
       };
 
-    environment.extraInit = mkIf cfg.useDaemon ''
+    environment.extraInit = ''
       # Set up secure multi-user builds: non-root users build through the
       # Nix daemon.
       if [ "$USER" != root -o ! -w /nix/var/nix/db ]; then

commit 53517162da6973aa72b141623b9a6946b4479797

nix: fix daemon check, only set if store is not writable
Fixes issues for a single-user install introduced in
9792b08fdf049524742143f4f788e3eb61a769fd.

modules/nix/default.nix
@@ -367,7 +367,7 @@ in
     environment.extraInit = ''
       # Set up secure multi-user builds: non-root users build through the
       # Nix daemon.
-      if [ "$USER" != root -o ! -w /nix/var/nix/db ]; then
+      if [ "$USER" != root -a ! -w /nix/var/nix/db ]; then
           export NIX_REMOTE=daemon
       fi
     '';