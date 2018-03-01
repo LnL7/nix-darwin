9a1499b12899157a2fb8b0e3394d96e8fff5e954
cmacrae <calum0macrae@gmail.com>
2018-03-01 13:20:16 +0000

    defaults: set screencapture.location's default to null

--- modules/system/defaults/screencapture.nix
+++ modules/system/defaults/screencapture.nix
@@ -6,8 +6,8 @@ with lib;
   options = {
 
     system.defaults.screencapture.location = mkOption {
-      type = types.string;
-      default = "~/Desktop";
+      type = types.nullOr types.str;
+      default = null;
       description = ''
           The filesystem path to which screencaptures should be written.
         '';
