577961c16222c0d31509265c4a2a3f475029ca60
Piotr Limanowski <piotr@codearsonist.com>
2018-03-03 15:25:43 +0100

    adds skhd module

---
+++ modules/services/skhd/default.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.skhd;
+
+in {
+
+  options = {
+    services.skhd.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Whether to enable the skhd hotkey daemon.";
+    };
+
+    services.skhd.package = mkOption {
+      type = types.package;
+      example = literalExample "pkgs.skhd";
+      description = "This option specifies the skhd package to use.";
+    };
+
+    services.skhd.skhdConfig = mkOption {
+      type = types.lines;
+      default = "";
+      example = "alt + shift - r   :   chunkc quit";
+      description = "Config to use for <filename>skhdrc</filename>.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    security.accessibilityPrograms = [ "${cfg.package}/bin/skhd" ];
+
+    environment.etc."skhdrc".text = cfg.skhdConfig;
+
+    launchd.user.agents.skhd = {
+      path = [ cfg.package config.environment.systemPath ];
+
+      serviceConfig.ProgramArguments = [ "${cfg.package}/bin/skhd" ]
+        ++ optionals (cfg.skhdConfig != "") [ "-c" "/etc/skhdrc" ];
+      serviceConfig.KeepAlive = true;
+      serviceConfig.ProcessType = "Interactive";
+      serviceConfig.StandardOutPath = "/tmp/skhd.out";
+      serviceConfig.StandardErrorPath = "/tmp/skhd.err";
+    };
+
+  };
+}
