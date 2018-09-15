6d5dcd60eb09182593652934a100104d7a4db0d0
Mario Rodas <marsam@users.noreply.github.com>
2018-09-15 15:27:41 -0500

    synapse-bt: Add basic test

---
+++ tests/services-synapse-bt.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  synapse-bt = pkgs.runCommand "synapse-bt-0.0.0" {} "mkdir $out";
+in
+
+{
+  services.synapse-bt.enable = true;
+  services.synapse-bt.package = synapse-bt;
+
+  test = ''
+    echo >&2 "checking synapse-bt service in ~/Library/LaunchAgents"
+    grep "org.nixos.synapse-bt" ${config.out}/user/Library/LaunchAgents/org.nixos.synapse-bt.plist
+    grep "${synapse-bt}/bin/synapse" ${config.out}/user/Library/LaunchAgents/org.nixos.synapse-bt.plist
+  '';
+}
