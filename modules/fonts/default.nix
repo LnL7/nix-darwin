b10e114e330ebdb23619b0a4c797a69ce9857e9a
Piotr Limanowski <piotr@codearsonist.com>
2018-09-30 14:37:31 +0200

    re-add fonts on every rebuild

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -23,15 +23,16 @@ in {
     };
   };
   
-  config = mkIf cfg.enableFontDir {
+  config = {
     system.build.fonts = pkgs.buildEnv {
       name = "system-fonts";
       paths = cfg.fonts;
       pathsToLink = "/share/fonts/truetype";
     };
-    system.activationScripts.fonts.text = ''
+    system.activationScripts.fonts.text = optionalString cfg.enableFontDir ''
       # Set up fonts.
       echo "setting up fonts..." >&2
+      fontrestore default -n 2>&1 | grep -o '/Library/Fonts/.*' | tr '\n' '\0' | xargs -0 sudo rm
       /bin/ln -hf ${config.system.build.fonts}/share/fonts/truetype/* /Library/Fonts/
       '';
     environment.pathsToLink = [ "/share/fonts/truetype" ];
