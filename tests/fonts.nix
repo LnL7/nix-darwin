5fa894c90ac224f86592a3d7da76932593542a16
Daiderd Jordan <daiderd@gmail.com>
2018-10-12 22:02:56 +0200

    tests: avoid depending on an actual font package

--- tests/fonts.nix
+++ tests/fonts.nix
@@ -1,17 +1,20 @@
 { config, pkgs, ... }:
 
 let
-  fonts = pkgs.runCommand "fonts-0.0.0" {} "mkdir -p $out";
-in {
-  fonts = {
-    enableFontDir = true;
-    fonts = [ pkgs.dejavu_fonts ];
-  };
+  font = pkgs.runCommand "font-0.0.0" {} ''
+    mkdir -p $out/share/fonts
+    touch $out/share/fonts/Font.ttf
+  '';
+in
+
+{
+  fonts.enableFontDir = true;
+  fonts.fonts = [ font ];
  
   test = ''
     echo checking installed fonts >&2
     grep -o "fontrestore default -n" ${config.out}/activate
-    grep -o "ln -fn '/run/current-system/sw/share/fonts/DejaVuSans.ttf' '/Library/Fonts/DejaVuSans.ttf'" ${config.out}/activate
+    grep -o "ln -fn '/run/current-system/sw/share/fonts/Font.ttf' '/Library/Fonts/Font.ttf'" ${config.out}/activate
   '';
 }
 
