a1c56d5492f7f97cc9aa18158033d5ae911bc317
Daiderd Jordan <daiderd@gmail.com>
2019-01-18 23:18:45 +0100

    system-packages: verify libraries are not linked
    By default environment.pathsToLink does not include /lib so libraries
    should not be exposed directly in the system output.

--- tests/system-packages.nix
+++ tests/system-packages.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 
+let
+  hello = pkgs.runCommand "hello-0.0.0" {} ''
+    mkdir -p $out/bin $out/lib
+    touch $out/bin/hello $out/lib/libhello.dylib
+  '';
+in
+
 {
    environment.systemPackages = [ pkgs.hello ];
 
    test = ''
      echo checking hello binary in /sw/bin >&2
+     test -e ${config.out}/sw/bin/hello
      test "$(readlink -f ${config.out}/sw/bin/hello)" = "${pkgs.hello}/bin/hello"
+
+     echo checking for unexpected paths in /sw/bin >&2
+     ! test -e ${config.out}/sw/lib/libhello.dylib
    '';
 }
