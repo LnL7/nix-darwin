76356b65d886c181c12cc130f679f1b268af4c88
Brian Hicks <brian@brianthicks.com>
2019-07-26 15:35:49 -0500

    symlink per-user applications to the right place

--- modules/system/applications.nix
+++ modules/system/applications.nix
@@ -6,6 +6,17 @@ let
 
   cfg = config.system;
 
+  packageUsers = filterAttrs (_: u: u.packages != []) config.users.users;
+
+  userApplications = mapAttrs (name: { packages, home, ... }: {
+    home = home;
+    applications = pkgs.buildEnv {
+      name = "${name}-applications";
+      paths = packages;
+      pathsToLink = "/Applications";
+    };
+  }) packageUsers;
+
 in
 
 {
@@ -29,6 +40,19 @@ in
       else
         echo "warning: /Applications/Nix Apps is a directory, skipping App linking..." >&2
       fi
+
+      ${concatStringsSep "\n" (mapAttrsToList (name: { applications, home }: ''
+        # Set up applications for ${name}
+        echo "setting up ${home}/Applications"
+
+        if [ ! -e ${home}/Applications -o -L ${home}/Applications ]; then
+          ln -sfn ${applications}/Applications ${home}/Applications
+        elif [ ! -e ${home}/Applications/Nix\ Apps -o -L ${home}/Applications/Nix\ Apps ]; then
+          ln -sfn ${applications}/Applications ${home}/Applications/Nix\ Apps
+        else
+          echo "warning: ${home}/Applications and ${home}/Applications/Nix Apps are directories, skipping App linking..." >&2
+        fi
+      '') userApplications)}
     '';
 
   };
