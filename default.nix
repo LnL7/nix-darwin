073935fb9994ccfaa30b658ace9feda2f8bbafee
Daiderd Jordan <daiderd@gmail.com>
2020-03-22 23:13:59 +0100

    fix evaluation of darwin.pkgs for recent nixpkgs
    The _module attribute was renamed in https://github.com/NixOS/nixpkgs/commits/dcdd232939232d04c1132b4cc242dd3dac44be8c.

--- default.nix
+++ default.nix
@@ -20,10 +20,14 @@ let
     specialArgs = { modulesPath = ./modules; };
     check = true;
   };
+
+  # Was moved in nixpkgs #82751, so both need to be handled here until 20.03 is deprecated.
+  # https://github.com/NixOS/nixpkgs/commits/dcdd232939232d04c1132b4cc242dd3dac44be8c
+  _module = eval._module or eval.config._module;
 in
 
 {
-  inherit (eval.config._module.args) pkgs;
+  inherit (_module.args) pkgs;
   inherit (eval) options config;
 
   system = eval.config.system.build.toplevel;
