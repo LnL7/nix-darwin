784b688dd92ae303dadee69e2063cfb7c4a29d9c
Łukasz Niemier <lukasz@niemier.pl>
2020-04-28 22:43:13 +0200

    docs: add example

--- modules/services/dnsmasq.nix
+++ modules/services/dnsmasq.nix
@@ -37,7 +37,10 @@ in
     services.dnsmasq.addresses = mkOption {
       type = types.attrs;
       default = {};
-      description = "List of domains that will be redirected by the DNSmasq";
+      description = "List of domains that will be redirected by the DNSmasq.";
+      example = literalExample ''
+        { localhost = "127.0.0.1"; }
+        '';
     };
   };
 
