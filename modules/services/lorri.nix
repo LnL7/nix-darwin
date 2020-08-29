30593350fd91f25b68bfa44cf5f948e481262258
Thibault Gagnaux <thibault@gagnaux.ch>
2020-08-29 16:32:49 +0200

    Makes it work.

--- modules/services/lorri.nix
+++ modules/services/lorri.nix
@@ -4,56 +4,43 @@ with lib;
 
 let
   cfg = config.services.lorri;
+  home = "${builtins.getEnv "HOME"}";
 in
 {
-  options = {
-    services.lorri.enable = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Whether to enable the lorri service.";
-    };
+  options =  {
+    services.lorri = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to enable the lorri service.";
+      };
 
-    services.lorri.package = mkOption {
-      type = types.path;
-      default = pkgs.lorri;
-      defaultText = "pkgs.lorri";
-      description = "This option specifies the lorri package to use.";
-    };
-
-    services.lorri.logFile = mkOption {
-      type = types.nullOr types.path;
-      default = "/var/tmp/lorri.log";
-      example =  "/var/tmp/lorri.log";
-      description = ''
-        The logfile to use for the lorri service. Alternatively
-        <command>sudo launchctl debug system/org.nixos.lorri --stderr</command>
-        can be used to stream the logs to a shell after restarting the service with
-        <command>sudo launchctl kickstart -k system/org.nixos.lorri</command>.
-      '';
-    };
-
-    services.lorri.tempDir = mkOption {
-      type = types.nullOr types.path;
-      default = null;
-      description = "The TMPDIR to use for lorri.";
+      logFile = mkOption {
+        type = types.nullOr types.path;
+        default = "${home}/Library/Logs/lorri.log";
+        example =  "${home}/Library/Logs/lorri.log";
+        description = ''
+          The logfile to use for the lorri service. Alternatively
+          <command>sudo launchctl debug system/com.target.lorri --stderr</command>
+          can be used to stream the logs to a shell after restarting the service with
+          <command>sudo launchctl kickstart -k system/com.target.lorri</command>.
+        '';
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ];
+    environment.systemPackages = [ pkgs.lorri ];
     launchd.user.agents.lorri = {
       serviceConfig = {
+        Label = "com.target.lorri";
+        ProgramArguments = with pkgs; ["${zsh}/bin/zsh" "-c" "${lorri}/bin/lorri daemon"];
         KeepAlive = true;
+        RunAtLoad = true;
         ProcessType = "Background";
-        LowPriorityIO = false;
         StandardOutPath = cfg.logFile;
         StandardErrorPath = cfg.logFile;
-        EnvironmentVariables = mkMerge [
-        config.nix.envVars
-        { TMPDIR = mkIf (cfg.tempDir != null) cfg.tempDir; }
-        ];
       };
-      command = "${cfg.package}/bin/lorri daemon";
     };
   };
 }
--- tests/services-lorri.nix
+++ tests/services-lorri.nix
@@ -2,17 +2,21 @@
 
 let
   nix = pkgs.runCommand "nix-0.0.0" { version = "1.11.6"; } "mkdir -p $out";
-  plistPath = "${config.out}/user/Library/LaunchAgents/org.nixos.lorri.plist";
+  plistPath = "${config.out}/user/Library/LaunchAgents/com.target.lorri.plist";
 in
 {
   services.lorri.enable = true;
-  nix.package = nix;
+  nix.package = pkgs.nix;
 
   test = ''
     echo checking lorri service in /Library/LaunchAgents >&2
-    cat ${plistPath}
     grep -o "<key>KeepAlive</key>" ${plistPath}
-    grep -o "<string>org.nixos.lorri</string>" ${plistPath}
-    grep -o "<string>exec ${pkgs.lorri}/bin/lorri daemon</string>" ${plistPath}
+    grep -o "<key>RunAtLoad</key>" ${plistPath}
+    grep -o "<key>ProcessType</key>" ${plistPath}
+    grep -o "<string>Background</string>" ${plistPath}
+    grep -o "<string>com.target.lorri</string>" ${plistPath}
+    grep -o "<string>${pkgs.zsh}/bin/zsh</string>" ${plistPath}
+    grep -o "<string>-c</string>" ${plistPath}
+    grep -o "<string>${pkgs.lorri}/bin/lorri daemon</string>" ${plistPath}
   '';
 }
