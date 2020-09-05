b8a2fd1c18c32f819e835b6cdb9ab0caebd8685d
Zach Coyle <zach.coyle@gmail.com>
2020-09-05 11:06:56 -0400

    Add trackpad dragging default

--- modules/system/defaults/trackpad.nix
+++ modules/system/defaults/trackpad.nix
@@ -13,6 +13,14 @@ with lib;
       '';
     };
 
+    system.defaults.trackpad.Dragging = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        Whether to enable tap-to-drag. The default is false.
+      '';
+    };
+
     system.defaults.trackpad.TrackpadRightClick = mkOption {
       type = types.nullOr types.bool;
       default = null;
