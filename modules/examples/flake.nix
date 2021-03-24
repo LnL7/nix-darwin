dbddb3a1a62f989bb92866b25369f71b89329ba5
Thibault Gagnaux <tgagnaux@gmail.com>
2021-03-24 20:46:18 +0100

    Adds `GITHUB_TOKEN` due to rate limiting

--- modules/examples/flake.nix
+++ modules/examples/flake.nix
@@ -2,7 +2,7 @@
   description = "Example darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-20.09-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs";
     darwin.url = "github:lnl7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
