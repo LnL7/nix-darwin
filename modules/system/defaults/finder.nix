d1c534422277b8f97f0323fc7e13309a12f8b064
Dan Yang <dsyang92@gmail.com>
2022-03-10 00:12:30 -0800

    option to show all hidden files

--- modules/system/defaults/finder.nix
+++ modules/system/defaults/finder.nix
@@ -5,6 +5,14 @@ with lib;
 {
   options = {
 
+    system.defaults.finder.AppleShowAllFiles = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        Whether to always show hidden files. The default is false.
+      '';
+    };
+
     system.defaults.finder.AppleShowAllExtensions = mkOption {
       type = types.nullOr types.bool;
       default = null;
