9a2978461a5a99928ded257be0b8c19e6dc27935
AlexOwl <AlexOwl@protonmail.com>
2022-08-27 01:17:24 +0400

    Create windowmanager.nix

---
+++ modules/system/defaults/windowmanager.nix
@@ -0,0 +1,43 @@
+{ config, lib, ... }:
+
+with lib;
+
+{
+  options = {
+    system.defaults.windowmanager.GloballyEnabled = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        # Apple menu > System Settings > Dekstop & Dock > Stage Manager
+        Stage Manager arranges your recent windows into a single strip for reduced clutter and quick access. Default is false.
+      '';
+    };
+
+    system.defaults.windowmanager.GloballyEnabledEver = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        # Apple menu > System Settings > Dekstop & Dock > Stage Manager
+        Default is true.
+      '';
+    };
+
+    system.defaults.windowmanager.AutoHide = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        # Apple menu > System Settings > Dekstop & Dock > Stage Manager
+        Do not show Recent Apps. Default is false.
+      '';
+    };
+
+    system.defaults.windowmanager.HideDesktop = mkOption {
+      type = types.nullOr types.bool;
+      default = null;
+      description = ''
+        # Apple menu > System Settings > Dekstop & Dock > Stage Manager
+        Hide dekstop items. Default is false.
+      '';
+    };
+  };
+}
