6b73d2c605cd1a063931f722151d00dc1157275d
Nathan Henrie <nate@n8henrie.com>
2022-10-20 14:49:26 -0600

    Type: TERMINFO_DIRS not TERMINFO

--- tests/environment-terminfo.nix
+++ tests/environment-terminfo.nix
@@ -5,6 +5,6 @@ with lib;
 {
   test = ''
     echo checking /usr/share/terminfo in environment >&2
-    grep 'export TERMINFO=.*:/usr/share/terminfo' ${config.system.build.setEnvironment}
+    grep 'export TERMINFO_DIRS=.*:/usr/share/terminfo' ${config.system.build.setEnvironment}
   '';
 }
