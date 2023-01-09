8fbdc6464ef5fcb47bb05be8386cf4b81e0098ea
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2023-01-08 21:40:44 -0700

    [MathSymbolsInput] v1.2 -> v1.3

--- modules/services/math-symbols-input/package/default.nix
+++ modules/services/math-symbols-input/package/default.nix
@@ -1,20 +1,13 @@
 { stdenv, fetchurl, xar, zip, cpio }:
 
-let
-  patch = fetchurl {
-    url =
-      "https://github.com/knrafto/MathSymbolsInput/commit/19352edd3e181d7ff3030460b3a87bbbaef7bccc.patch";
-    sha256 = "sha256-0q0PJE3ZxaIHof5o6epgncMtgrVBWBIm+CZbkfl1Z2E=";
-  };
-
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "MathSymbolsInput";
-  version = "v1.2";
+  version = "v1.3";
 
   src = fetchurl {
     url =
       "https://github.com/knrafto/${pname}/releases/download/${version}/${pname}.pkg";
-    sha256 = "sha256-/zQ9PEP9qO6fc77PcWeHMJa7gICyJL6UmuoZPNezDVc=";
+    sha256 = "sha256-+s+TFoAv2rWl3gOHp8LLUZm+OTMDxgZtK0vmHQDJzFU=";
   };
 
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
@@ -24,7 +17,6 @@ in stdenv.mkDerivation rec {
 
   buildPhase = ''
     cat MathSymbolsInput.pkg/Payload  | gunzip -dc | cpio -i
-    patch -u Math\ Symbols\ Input.app/Contents/Resources/commands.txt -i ${patch}
   '';
 
   installPhase = ''
