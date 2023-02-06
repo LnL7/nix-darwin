d35937c65dc2f553bb05b54ccefa3c958f08726c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-02-05 21:10:29 -0500

    Add enableTmuxTouchIdAuth flag

--- modules/security/pam.nix
+++ modules/security/pam.nix
@@ -15,49 +15,71 @@ let
   # As such, we resort to line addition/deletion in place using `sed`. We add a comment to the
   # added line that includes the name of the option, to make it easier to identify the line that
   # should be deleted when the option is disabled.
-  mkSudoTouchIdAuthScript = isEnabled:
-  let
-    file   = "/etc/pam.d/sudo";
-    option = "security.pam.enableSudoTouchIdAuth";
-    sed = "${pkgs.gnused}/bin/sed";
-  in ''
-    ${if isEnabled then ''
-      # Enable sudo Touch ID authentication, if not already enabled
-      if ! grep 'pam_tid.so' ${file} > /dev/null; then
-        ${sed} -i '2i\
-      auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so # nix-darwin: ${option} \
-      auth       sufficient     pam_tid.so # nix-darwin: ${option}
-        ' ${file}
-      fi
-    '' else ''
-      # Disable sudo Touch ID authentication, if added by nix-darwin
-      if grep '${option}' ${file} > /dev/null; then
-        ${sed} -i '/${option}/d' ${file}
-      fi
-    ''}
-  '';
+  mkSudoTouchIdAuthScript = cfg:
+    let
+      disableTmuxTouchIdAuth = ''
+        # Disable tmux Touch ID authentication, if added by nix-darwin
+        if grep '${optionTmux}' ${file} > /dev/null; then
+          ${sed} -i '/${optionTmux}/d' ${file}
+        fi
+      '';
+      file = "/etc/pam.d/sudo";
+      optionSudo = "security.pam.enableSudoTouchIdAuth";
+      optionTmux = "security.pam.enableTmuxTouchIdAuth";
+      sed = "${pkgs.gnused}/bin/sed";
+    in
+    ''
+      ${if cfg.enableSudoTouchIdAuth then ''
+        # Enable sudo Touch ID authentication, if not already enabled
+        if ! grep 'pam_tid.so' ${file} > /dev/null; then
+          ${sed} -i '2i\
+        auth       sufficient     pam_tid.so # nix-darwin: ${optionSudo}
+          ' ${file}
+        fi
+        ${if cfg.enableTmuxTouchIdAuth then ''
+        # Enable tmux Touch ID authentication, if not already enabled
+        if ! grep 'pam_reattach.so' ${file} > /dev/null; then
+          ${sed} -i '2i\
+        auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so # nix-darwin: ${optionTmux}
+          ' ${file}
+        fi
+        '' else disableTmuxTouchIdAuth
+        }
+      '' else ''
+        ${disableTmuxTouchIdAuth}
+        # Disable sudo Touch ID authentication, if added by nix-darwin
+        if grep '${optionSudo}' ${file} > /dev/null; then
+          ${sed} -i '/${optionSudo}/d' ${file}
+        fi
+      ''}
+    '';
 in
 
 {
   options = {
-    security.pam.enableSudoTouchIdAuth = mkEnableOption ''
-      Enable sudo authentication with Touch ID
+    security.pam = {
+      enableSudoTouchIdAuth = mkEnableOption ''
+        Enable sudo authentication with Touch ID
 
-      When enabled, this option adds the following line to /etc/pam.d/sudo:
+        When enabled, this option adds the following line to /etc/pam.d/sudo:
 
-          auth       sufficient     pam_tid.so
+            auth       sufficient     pam_tid.so
 
-      (Note that macOS resets this file when doing a system update. As such, sudo
-      authentication with Touch ID won't work after a system update until the nix-darwin
-      configuration is reapplied.)
-    '';
+        (Note that macOS resets this file when doing a system update. As such, sudo
+        authentication with Touch ID won't work after a system update until the nix-darwin
+        configuration is reapplied.)
+      '';
+      enableTmuxTouchIdAuth = mkEnableOption ''
+        TODO: Depends on enableSudoTouchIdAuth
+      '';
+    };
   };
 
   config = {
     system.activationScripts.pam.text = ''
       # PAM settings
       echo >&2 "setting up pam..."
-      ${mkSudoTouchIdAuthScript cfg.enableSudoTouchIdAuth}
+      ${mkSudoTouchIdAuthScript cfg}
     '';
   };
 }
