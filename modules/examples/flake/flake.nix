f532e43f7efa58a0202956fac19f402893ae0ecd
Michael Hoang <enzime@users.noreply.github.com>
2023-06-12 11:01:21 +1000

    templates.flake: add contents of simple.nix

--- modules/examples/flake/flake.nix
+++ modules/examples/flake/flake.nix
@@ -10,18 +10,33 @@
   outputs = { self, darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      nix.package = pkgs.nixVersions.stable;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
 
-      # FIXME: for github actions, this shouldn't be in the example.
+      # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
     };
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake ./modules/examples#simple \
-    #       --override-input darwin .
+    # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."simple" = darwin.lib.darwinSystem {
-      modules = [ configuration darwin.darwinModules.simple ];
+      modules = [ configuration ];
       system = "x86_64-darwin";
     };
 
