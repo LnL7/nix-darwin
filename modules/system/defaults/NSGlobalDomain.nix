8959735a60f6d41b46ae023661ca737d0f017355
Rob Harrop <rob@robharrop.dev>
2024-02-17 15:33:23 +0000

    Add NSStatusItemSpacing and NSStatusItemSelectionPadding NSGlobalDomain options

--- modules/system/defaults/NSGlobalDomain.nix
+++ modules/system/defaults/NSGlobalDomain.nix
@@ -232,6 +232,24 @@ in {
         Whether to enable moving window by holding anywhere on it like on Linux. The default is false.
       '';
     };
+    
+    system.defaults.NSGlobalDomain.NSStatusItemSpacing = mkOption {
+      type = types.nullOr types.int;
+      default = null;
+      example = 12;
+      description = lib.mdDoc ''
+        Sets the spacing between status icons in the menu bar. The default is null.
+      '';
+    };
+
+    system.defaults.NSGlobalDomain.NSStatusItemSelectionPadding = mkOption {
+      type = types.nullOr types.int;
+      default = null;
+      example = 6;
+      description = lib.mdDoc ''
+        Sets the padding around status icons in the menu bar. The default is null.
+      '';
+    };
 
     system.defaults.NSGlobalDomain.InitialKeyRepeat = mkOption {
       type = types.nullOr types.int;
