9f25d2873ec9a834332f4b714dfb1717d4c2637f
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-02-26 20:25:40 -0800

    Potential first version of security.wrappers

--- modules/system/activation-scripts.nix
+++ modules/system/activation-scripts.nix
@@ -70,6 +70,7 @@ in
       ${cfg.activationScripts.keyboard.text}
       ${cfg.activationScripts.fonts.text}
       ${cfg.activationScripts.nvram.text}
+      ${cfg.activationScripts.wrappers.text}
 
       ${cfg.activationScripts.postActivation.text}
 
