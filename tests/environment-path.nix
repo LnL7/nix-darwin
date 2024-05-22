bd0ed8599fd4871e79543e09075dac2c2c25ff2a
Andrew Marshall <andrew@johnandrewmarshall.com>
2024-05-22 13:43:33 -0400

    environment: Test how order of systemPath and profiles manifests

--- tests/environment-path.nix
+++ tests/environment-path.nix
@@ -3,19 +3,37 @@
 with lib;
 
 {
+  environment.systemPath = mkMerge [
+    (mkBefore [ "beforePath" ])
+    [ "myPath" ]
+    (mkAfter [ "afterPath" ])
+  ];
+
+  environment.profiles = mkMerge [
+    (mkBefore [ "beforeProfile" ])
+    [ "myProfile" ]
+    (mkAfter [ "afterProfile" ])
+  ];
+
   test = ''
     echo 'checking PATH' >&2
     env_path=$(bash -c 'source ${config.system.build.setEnvironment}; echo $PATH')
 
     test "$env_path" = "${builtins.concatStringsSep ":" [
+      "beforePath"
+      "myPath"
+      "beforeProfile/bin"
       "/homeless-shelter/.nix-profile/bin"
+      "myProfile/bin"
       "/run/current-system/sw/bin"
       "/nix/var/nix/profiles/default/bin"
+      "afterProfile/bin"
       "/usr/local/bin"
       "/usr/bin"
       "/usr/sbin"
       "/bin"
       "/sbin"
+      "afterPath"
     ]}"
   '';
 }
