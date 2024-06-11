1c81ad717aff0c9a0b061fb0eb24a52e71c1f921
Prav!n <50878560+amsynist@users.noreply.github.com>
2024-06-12 00:02:21 +0530

    module: add jankyborders service

---
+++ modules/services/jankyborders/default.nix
@@ -0,0 +1,148 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) maintainers mkEnableOption mkIf mkPackageOptionMD mkOption types;
+
+  cfg = config.services.jankyborders;
+  optionalArg = arg: value:
+    if value != null && value != ""
+    then
+      if lib.isList value
+      then lib.map (val: "${arg}=${val}") value
+      else ["${arg}=${value}"]
+    else [];
+in {
+  meta.maintainers = [
+    maintainers.amsynist or "amsynist"
+  ];
+
+  options.services.jankyborders = {
+    enable = mkEnableOption "Enable the jankyborders service.";
+
+    package = mkPackageOptionMD pkgs "jankyborders" {};
+
+    width = mkOption {
+      type = types.float;
+      default = 5.0;
+      description = ''
+        Determines the width of the border. For example, width=5.0 creates a border 5.0 points wide.
+      '';
+    };
+
+    hidpi = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        If set to on, the border will be drawn with retina resolution.
+      '';
+    };
+
+    active_color = mkOption {
+      type = types.str;
+      default = "0xFFFFFFFF";
+      example = "0xFFFFFFFF";
+      description = ''
+        Sets the border color for the focused window (format: 0xAARRGGBB). For instance, active_color="0xff00ff00" creates a green border.
+        For Gradient Border : active_color="gradient(top_right=0x9992B3F5,bottom_left=0x9992B3F5)"
+      '';
+    };
+
+    inactive_color = mkOption {
+      type = types.str;
+      default = "0xFFFFFFFF";
+      example = "0xFFFFFFFF";
+      description = ''
+        Sets the border color for all windows not in focus (format: 0xAARRGGBB).
+        For Gradient Border : inactive_color="gradient(top_right=0x9992B3F5,bottom_left=0x9992B3F5)"
+      '';
+    };
+
+    background_color = mkOption {
+      type = types.str;
+      default = "";
+      example = "0xFFFFFFFF";
+      description = ''
+        Sets the background fill color for all windows (only 0xAARRGGBB arguments supported).
+      '';
+    };
+
+    style = mkOption {
+      type = types.str;
+      default = "round";
+      example = "square/round";
+      description = ''
+        Specifies the style of the border (either round or square).
+      '';
+    };
+
+    blur_radius = mkOption {
+      type = types.float;
+      default = 0.0;
+      example = 5.0;
+      description = ''
+        Sets the blur radius applied to the borders or backgrounds with transparency.
+      '';
+    };
+
+    ax_focus = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        If set to true, the (slower) accessibility API is used to resolve the focused window.
+      '';
+    };
+
+    blacklist = mkOption {
+      type = types.str;
+      default = "";
+      example = "Safari,kitty";
+      description = ''
+        The applications specified here are excluded from being bordered. For example, blacklist="Safari,kitty" excludes Safari and kitty from being bordered.
+      '';
+    };
+
+    whitelist = mkOption {
+      type = types.str;
+      default = "";
+      example = "Safari,kitty";
+      description = ''
+        Once this list is populated, only applications listed here are considered for receiving a border. If the whitelist is empty (default) it is inactive.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [cfg.package];
+
+    launchd.user.agents.jankyborders = {
+      path = [config.environment.systemPath];
+      serviceConfig.ProgramArguments =
+        [
+          "${cfg.package}/bin/borders"
+        ]
+        ++ (optionalArg "width" (toString cfg.width))
+        ++ (optionalArg "hidpi" (
+          if cfg.hidpi
+          then "on"
+          else "off"
+        ))
+        ++ (optionalArg "active_color" cfg.active_color)
+        ++ (optionalArg "inactive_color" cfg.inactive_color)
+        ++ (optionalArg "background_color" cfg.background_color)
+        ++ (optionalArg "style" cfg.style)
+        ++ (optionalArg "blur_radius" (toString cfg.blur_radius))
+        ++ (optionalArg "ax_focus" (
+          if cfg.ax_focus
+          then "on"
+          else "off"
+        ))
+        ++ (optionalArg "blacklist" cfg.blacklist)
+        ++ (optionalArg "whitelist" cfg.whitelist);
+      serviceConfig.KeepAlive = true;
+      serviceConfig.RunAtLoad = true;
+    };
+  };
+}
