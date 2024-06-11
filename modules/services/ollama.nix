30029ae0b28d2d55b1372809195fbfbab9afca06
Velnbur <kyrylo.baybula@distributedlab.com>
2024-06-11 12:22:04 +0300

    feat: Add `ollama` service

---
+++ modules/services/ollama.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.ollama;
+
+in {
+  options = {
+    services.ollama = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Whether to enable the Ollama Daemon.";
+      };
+
+      package = mkOption {
+        type = types.path;
+        default = pkgs.ollama;
+        description = "This option specifies the ollama package to use.";
+      };
+
+      exec = mkOption {
+        type = types.str;
+        default = "ollama";
+        description = "Ollama command/binary to execute.";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    launchd.user.agents.ollama = {
+      path = [ config.environment.systemPath ];
+      serviceConfig.ProgramArguments =
+        [ "${cfg.package}/bin/${cfg.exec}" "serve" ];
+      serviceConfig.RunAtLoad = true;
+    };
+  };
+}
