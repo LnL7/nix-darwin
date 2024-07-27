4f2d4a01ed07bbc277e4f4c6ca8055e773a0e415
Andrew Lubawy <andrew@andrewlubawy.com>
2024-07-26 21:06:23 -0700

    Remove implementation note a cleanup description

--- modules/security/pam.nix
+++ modules/security/pam.nix
@@ -4,17 +4,6 @@ with lib;
 
 let
   cfg = config.security.pam;
-
-  # Implementation Notes
-  #
-  # We don't use `environment.etc` because this would require that the user manually delete
-  # `/etc/pam.d/sudo` which seems unwise given that applying the nix-darwin configuration requires
-  # sudo. We also can't use `system.patchs` since it only runs once, and so won't patch in the
-  # changes again after OS updates (which remove modifications to this file).
-  #
-  # As such, we resort to line addition/deletion in place using `sed`. We add a comment to the
-  # added line that includes the name of the option, to make it easier to identify the line that
-  # should be deleted when the option is disabled.
 in
 {
   options = {
@@ -28,16 +17,18 @@ in
           ```
         '';
       };
-      enablePamReattach = mkEnableOption ''
-        Enable re-attaching a program to the user's bootstrap session.
-        This allows programs like tmux and screen that run in the background to
-        survive across user sessions to work with PAM services that are tied to the
-        bootstrap session.
-        When enabled, this option adds the following line to /etc/pam.d/sudo_local:
-        ```
-        auth       optional       /path/in/nix/store/lib/pam/pam_reattach.so"
-        ```
-      '';
+      enablePamReattach = mkEnableOption "" // {
+        description = ''
+          Enable re-attaching a program to the user's bootstrap session.
+          This allows programs like tmux and screen that run in the background to
+          survive across user sessions to work with PAM services that are tied to the
+          bootstrap session.
+          When enabled, this option adds the following line to /etc/pam.d/sudo_local:
+          ```
+          auth       optional       /path/in/nix/store/lib/pam/pam_reattach.so"
+          ```
+        '';
+      };
     };
   };
 
