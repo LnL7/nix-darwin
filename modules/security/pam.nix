d3fbe2b54a6b02fa0276c9e46726a417e0e82d4f
Andrew Lubawy <andrew@andrewlubawy.com>
2024-07-26 18:05:15 -0700

    Fix pam module with sudo_local and add reattach for tmux

--- modules/security/pam.nix
+++ modules/security/pam.nix
@@ -15,21 +15,46 @@ let
   # As such, we resort to line addition/deletion in place using `sed`. We add a comment to the
   # added line that includes the name of the option, to make it easier to identify the line that
   # should be deleted when the option is disabled.
-  mkSudoTouchIdAuthScript = isEnabled:
-  let
-    file   = "/etc/pam.d/sudo";
-    option = "security.pam.enableSudoTouchIdAuth";
-    sed = "${pkgs.gnused}/bin/sed";
-  in ''
+  template   = "/etc/pam.d/sudo_local.template";
+  file   = "/etc/pam.d/sudo_local";
+  sed = "${pkgs.gnused}/bin/sed";
+  mkSudoLocal = isEnabled:
+  ''
     ${if isEnabled then ''
-      # Enable sudo Touch ID authentication, if not already enabled
-      if ! grep 'pam_tid.so' ${file} > /dev/null; then
-        ${sed} -i '2i\
-      auth       sufficient     pam_tid.so # nix-darwin: ${option}
-        ' ${file}
+      if [ ! -f ${file} ]; then
+        cp ${template} ${file}
       fi
     '' else ''
-      # Disable sudo Touch ID authentication, if added by nix-darwin
+      if [ -f ${file} ]; then
+        rm ${file}
+      fi
+    ''}
+  '';
+  mkSudoTouchIdAuthScript = isEnabled:
+  let
+    option = "security.pam.enableSudoTouchIdAuth";
+  in
+  ''
+    ${if isEnabled then ''
+      if ! grep '${option}' ${file} > /dev/null; then
+        ${sed} -i '2iauth       sufficient     pam_tid.so # nix-darwin: ${option}' ${file}
+      fi
+    '' else ''
+      if grep '${option}' ${file} > /dev/null; then
+        ${sed} -i '/${option}/d' ${file}
+      fi
+    ''}
+  '';
+  mkPamReattachScript = isEnabled:
+  let
+    option = "security.pam.enablePamReattach";
+  in
+  ''
+    ${if isEnabled then ''
+      if ! grep '${option}' ${file} > /dev/null; then
+        ${sed} -i '1iauth optional ${pkgs.pam-reattach}/lib/pam/pam_reattach.so # nix-darwin: ${option}' ${file}
+      fi
+    '' else ''
       if grep '${option}' ${file} > /dev/null; then
         ${sed} -i '/${option}/d' ${file}
       fi
@@ -39,22 +64,31 @@ in
 
 {
   options = {
-    security.pam.enableSudoTouchIdAuth = mkEnableOption "" // {
+    security.pam = {
+    enableSudoTouchIdAuth = mkEnableOption "" // {
       description = ''
         Enable sudo authentication with Touch ID.
 
-        When enabled, this option adds the following line to
-        {file}`/etc/pam.d/sudo`:
+        When enabled, this option creates {file}
+        from {template}:
 
         ```
+        # sudo_local: local config file which survives system update and is included for sudo
+        # uncomment following line to enable Touch ID for sudo
         auth       sufficient     pam_tid.so
         ```
-
-        ::: {.note}
-        macOS resets this file when doing a system update. As such, sudo
-        authentication with Touch ID won't work after a system update
-        until the nix-darwin configuration is reapplied.
-        :::
+      '';
+    };
+    enablePamReattach = mkEnableOption ''
+        Enable re-attaching a program to the user's bootstrap session.
+        This allows programs like tmux and screen that run in the background to
+        survive across user sessions to work with PAM services that are tied to the
+        bootstrap session.
+        When enabled, this option adds the following line to /etc/pam.d/sudo_local:
+            auth       optional       /path/in/nix/store/lib/pam/pam_reattach.so"
+        (Note that macOS resets this file when doing a system update. As such, sudo
+        authentication with Touch ID won't work after a system update until the nix-darwin
+        configuration is reapplied.)
       '';
     };
   };
@@ -63,6 +97,8 @@ in
     system.activationScripts.pam.text = ''
       # PAM settings
       echo >&2 "setting up pam..."
+      ${mkSudoLocal (cfg.enableSudoTouchIdAuth || cfg.enablePamReattach)}
+      ${mkPamReattachScript cfg.enablePamReattach}
       ${mkSudoTouchIdAuthScript cfg.enableSudoTouchIdAuth}
     '';
   };
