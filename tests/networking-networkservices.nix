d7efcbce641cc1b060cc1e557fab05d668d1481f
Theodore Ni <3806110+tjni@users.noreply.github.com>
2024-08-26 18:23:12 -0700

    Support configuring network locations
    This adds support for configuring DNS servers and search domains for
    each location. This is exposed through the networking.location option.
    
    The networking.knownNetworkServices option now applies globally across
    all locations.
    
    The networking.dns and networking.search options are now aliases under
    networking.location.Automatic, since the "Automatic" network location
    is the default location on an unconfigured system.

--- tests/networking-networkservices.nix
+++ tests/networking-networkservices.nix
@@ -1,15 +1,35 @@
 { config, lib, ... }:
 
 {
-  networking.knownNetworkServices = [ "Wi-Fi" "Thunderbolt Ethernet" ];
-  networking.dns = [ "8.8.8.8" "8.8.4.4" ];
+  networking.knownNetworkServices = [
+    "Wi-Fi"
+    "Thunderbolt Ethernet"
+  ];
+  networking.dns = [
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
+  networking.location."Home Lab" = {
+    search = [ "home.lab" ];
+  };
 
   test = ''
     echo checking dns settings in /activate >&2
+
+    grep "networksetup -switchtolocation ${lib.escapeShellArg "Automatic"}" ${config.out}/activate
     grep "networksetup -setdnsservers ${lib.escapeShellArgs [ "Wi-Fi" "8.8.8.8" "8.8.4.4" ]}" ${config.out}/activate
     grep "networksetup -setdnsservers ${lib.escapeShellArgs [ "Thunderbolt Ethernet" "8.8.8.8" "8.8.4.4" ]}" ${config.out}/activate
-    echo checking empty searchdomain settings in /activate >&2
+
+    grep "networksetup -switchtolocation ${lib.escapeShellArg "Home Lab"}" ${config.out}/activate
+    grep "networksetup -setdnsservers ${lib.escapeShellArgs [ "Wi-Fi" "empty" ]}" ${config.out}/activate
+    grep "networksetup -setdnsservers ${lib.escapeShellArgs [ "Thunderbolt Ethernet" "empty" ]}" ${config.out}/activate
+
+    echo checking searchdomain settings in /activate >&2
+
     grep "networksetup -setsearchdomains ${lib.escapeShellArgs [ "Wi-Fi" "empty" ]}" ${config.out}/activate
     grep "networksetup -setsearchdomains ${lib.escapeShellArgs [ "Thunderbolt Ethernet" "empty" ]}" ${config.out}/activate
+
+    grep "networksetup -setsearchdomains ${lib.escapeShellArgs [ "Wi-Fi" "home.lab" ]}" ${config.out}/activate
+    grep "networksetup -setsearchdomains ${lib.escapeShellArgs [ "Thunderbolt Ethernet" "home.lab" ]}" ${config.out}/activate
   '';
 }
