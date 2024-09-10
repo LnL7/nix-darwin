15f64efcaf936f3b77955018d29b4802be6b144f
Sebastian Reuße <seb@schattenkopie.de>
2024-09-10 21:40:11 +0200

    zsh: prefer Nix completions these from Zsh package
    Zsh ships some rudimentary completions for programs where upstream also
    ships their own completions (e.g., curl). So as not to shadow those
    completions, we need to prepend to the fpath instead of appending.
    
    NixOS/nixpkgs@8dad5a22399782a4ef681174219546cb050e580f

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -137,7 +137,7 @@ in
 
       # Tell zsh how to find installed completions
       for p in ''${(z)NIX_PROFILES}; do
-        fpath+=($p/share/zsh/site-functions $p/share/zsh/$ZSH_VERSION/functions $p/share/zsh/vendor-completions)
+        fpath=($p/share/zsh/site-functions $p/share/zsh/$ZSH_VERSION/functions $p/share/zsh/vendor-completions $fpath)
       done
 
       ${cfg.shellInit}
