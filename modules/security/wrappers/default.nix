5ce52286fe16eff5c0580aa5001853633fca330f
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-11-07 10:06:18 -0800

    security.wrappers: unset Darwin-specific env vars

--- modules/security/wrappers/default.nix
+++ modules/security/wrappers/default.nix
@@ -54,23 +54,6 @@ let
 
   securityWrapper = sourceProg : pkgs.pkgsStatic.callPackage ./wrapper.nix {
     inherit sourceProg;
-
-    # glibc definitions of insecure environment variables
-    #
-    # We extract the single header file we need into its own derivation,
-    # so that we don't have to pull full glibc sources to build wrappers.
-    #
-    # They're taken from pkgs.glibc so that we don't have to keep as close
-    # an eye on glibc changes. Not every relevant variable is in this header,
-    # so we maintain a slightly stricter list in wrapper.c itself as well.
-    # unsecvars = lib.overrideDerivation (pkgs.srcOnly pkgs.glibc)
-    #   ({ name, ... }: {
-    #     name = "${name}-unsecvars";
-    #     installPhase = ''
-    #       mkdir $out
-    #       cp sysdeps/generic/unsecvars.h $out
-    #     '';
-    #   });
   };
 
   mkWrapper =
