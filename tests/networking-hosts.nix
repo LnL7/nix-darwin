9129a1b0192f8496d03276f1d13390aa88164403
ibizaman <pierre@tiserbox.com>
2024-12-04 11:20:10 +0100

    fix test

--- tests/networking-hosts.nix
+++ tests/networking-hosts.nix
@@ -11,16 +11,24 @@
 
     file=${config.out}/etc/hosts
 
-    if [[ ! $(grep '127.0.0.1' $file | head -n1) =~ "localhost$" ]]; then
+    if [[ ! $(grep '127.0.0.1' $file | head -n1) =~ localhost$ ]]; then
+      cat $file
+      echo "'$(grep '127.0.0.1' $file | head -n1)'"
       exit 1
     fi
-    if [[ ! $(grep '127.0.0.1' $file | tail -n1) =~ "my.super.host$" ]]; then
+    if [[ ! $(grep '127.0.0.1' $file | tail -n1) =~ my.super.host$ ]]; then
+      cat $file
+      echo "'$(grep '127.0.0.1' $file | tail -n1)'"
       exit 2
     fi
-    if [[ ! $(grep '::1' $file) =~ "localhost$" ]]; then
-      exit 2
+    if [[ ! $(grep '::1' $file) =~ localhost$ ]]; then
+      cat $file
+      echo "'$(grep '::1' $file)'"
+      exit 3
     fi
-    if [[ ! $(grep '10.0.0.1' $file) =~ "my.super.host my.other.host$"]]; then
+    if [[ ! $(grep '10.0.0.1' $file) =~ my.super.host\ my.other.host$ ]]; then
+      cat $file
+      echo "'$(grep '10.0.0.1' $file)'"
       exit 4
     fi
   '';
