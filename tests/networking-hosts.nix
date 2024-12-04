1189bcdf7c640e5fe951b8c5e64a82c99286fe1e
ibizaman <pierre@tiserbox.com>
2024-12-04 08:50:08 +0100

    add more tests

--- tests/networking-hosts.nix
+++ tests/networking-hosts.nix
@@ -10,7 +10,18 @@
     echo checking /etc/hosts file >&2
 
     file=${config.out}/etc/hosts
-    grep '127.0.0.1' $file
-    grep '10.0.0.1 my.super.host my.other.host' $file
+
+    if [[ ! $(grep '127.0.0.1' $file | head -n1) =~ "localhost$" ]]; then
+      exit 1
+    fi
+    if [[ ! $(grep '127.0.0.1' $file | tail -n1) =~ "my.super.host$" ]]; then
+      exit 2
+    fi
+    if [[ ! $(grep '::1' $file) =~ "localhost$" ]]; then
+      exit 2
+    fi
+    if [[ ! $(grep '10.0.0.1' $file) =~ "my.super.host my.other.host$"]]; then
+      exit 4
+    fi
   '';
 }
