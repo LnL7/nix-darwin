4e9943b2ca4f3c7c9b12531501a4fd9401552ad9
Emily <vcs@emily.moe>
2025-01-08 19:56:52 +0000

    nix-darwin 24.11

--- modules/examples/flake/flake.nix
+++ modules/examples/flake/flake.nix
@@ -2,8 +2,8 @@
   description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
