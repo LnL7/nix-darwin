e33d37c41f8040631f0cc16b032a1cf214aeeb4e
Emily <vcs@emily.moe>
2025-01-08 19:10:22 +0000

    {readme,examples/flake}: update for release branches

--- modules/examples/flake/flake.nix
+++ modules/examples/flake/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
