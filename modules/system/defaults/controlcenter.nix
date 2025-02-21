85d8a5f20a7ae549fd72fd4785061b0c04790e98
angel <78835633+Iamanaws@users.noreply.github.com>
2025-02-21 13:34:53 -0800

    controlcenter: format with nixfmt-rfc-style

--- modules/system/defaults/controlcenter.nix
+++ modules/system/defaults/controlcenter.nix
@@ -1,16 +1,34 @@
 { config, lib, ... }:
 let
-  mkEnumApply = mapping: default: v:
-    if v == null then null
-    else if mapping ? v then mapping.${v} else default;
-  mkBoolApply = mapping: v:
-    if v == null then null
-    else if v then mapping.true else mapping.false;
-in {
+  mkEnumApply =
+    mapping: default: v:
+    if v == null then
+      null
+    else if mapping ? v then
+      mapping.${v}
+    else
+      default;
+  mkBoolApply =
+    mapping: v:
+    if v == null then
+      null
+    else if v then
+      mapping.true
+    else
+      mapping.false;
+in
+{
   options = {
 
     system.defaults.controlcenter.AccessibilityShortcuts = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 3;
@@ -19,32 +37,42 @@ in {
         hide = 12;
       } 12;
       description = ''
-          Apple menu > System Preferences > Control Center > Accessibility Shortcuts
+        Apple menu > System Preferences > Control Center > Accessibility Shortcuts
 
-          Options:
-            both           - Show in Menu Bar and Control Center (3)
-            menuBar        - Show in Menu Bar only (6)
-            controlCenter  - Show in Control Center only (1 or 9)
-            hide           - Don't Show (2, 4, 8, or 12)
+        Options:
+          both           - Show in Menu Bar and Control Center (3)
+          menuBar        - Show in Menu Bar only (6)
+          controlCenter  - Show in Control Center only (1 or 9)
+          hide           - Don't Show (2, 4, 8, or 12)
       '';
     };
 
     system.defaults.controlcenter.AirDrop = lib.mkOption {
       type = lib.types.nullOr lib.types.bool;
       default = null;
-      apply = mkBoolApply { true = 18; false = 24; };
+      apply = mkBoolApply {
+        true = 18;
+        false = 24;
+      };
       description = ''
-          Apple menu > System Preferences > Control Center > AirDrop
+        Apple menu > System Preferences > Control Center > AirDrop
 
-          Show a AirDrop control in menu bar. Default is null.
+        Show a AirDrop control in menu bar. Default is null.
 
-          18 = Show in Menu Bar
-          24 = Don't Show in Menu Bar
+        18 = Show in Menu Bar
+        24 = Don't Show in Menu Bar
       '';
     };
 
     system.defaults.controlcenter.Battery = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 3;
@@ -53,13 +81,13 @@ in {
         hide = 12;
       } 4;
       description = ''
-          Apple menu > System Preferences > Control Center > Battery
+        Apple menu > System Preferences > Control Center > Battery
 
-          Options:
-            both           - Show in Menu Bar and Control Center (3)
-            menuBar        - Show in Menu Bar only (6)
-            controlCenter  - Show in Control Center only (1 or 9)
-            hide           - Don't Show (2, 4, 8, or 12)
+        Options:
+          both           - Show in Menu Bar and Control Center (3)
+          menuBar        - Show in Menu Bar only (6)
+          controlCenter  - Show in Control Center only (1 or 9)
+          hide           - Don't Show (2, 4, 8, or 12)
       '';
     };
 
@@ -67,12 +95,12 @@ in {
       type = lib.types.nullOr lib.types.bool;
       default = null;
       description = ''
-          Apple menu > System Preferences > Control Center > Battery
+        Apple menu > System Preferences > Control Center > Battery
 
-          Show a battery energy mode. Default is null.
+        Show a battery energy mode. Default is null.
 
-          false - "When Active"
-          true  - "Always"
+        false - "When Active"
+        true  - "Always"
       '';
     };
 
@@ -80,28 +108,37 @@ in {
       type = lib.types.nullOr lib.types.bool;
       default = null;
       description = ''
-          Apple menu > System Preferences > Control Center > Battery
+        Apple menu > System Preferences > Control Center > Battery
 
-          Show a battery percentage in menu bar. Default is null.
+        Show a battery percentage in menu bar. Default is null.
       '';
     };
 
     system.defaults.controlcenter.Bluetooth = lib.mkOption {
       type = lib.types.nullOr lib.types.bool;
       default = null;
-      apply = mkBoolApply { true = 18; false = 24; };
+      apply = mkBoolApply {
+        true = 18;
+        false = 24;
+      };
       description = ''
-          Apple menu > System Preferences > Control Center > Bluetooth
+        Apple menu > System Preferences > Control Center > Bluetooth
 
-          Show a bluetooth control in menu bar. Default is null.
+        Show a bluetooth control in menu bar. Default is null.
 
-          18 = Show in Menu Bar
-          24 = Don't Show in Menu Bar
+        18 = Show in Menu Bar
+        24 = Don't Show in Menu Bar
       '';
     };
 
     system.defaults.controlcenter.Display = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "whenActive" "hide" "always" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "whenActive"
+          "hide"
+          "always"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         always = 18;
@@ -109,17 +146,23 @@ in {
         whenActive = 2;
       } 2;
       description = ''
-          Apple menu > System Preferences > Control Center > Display
+        Apple menu > System Preferences > Control Center > Display
 
-          Options:
-            whenActive  - Show When Active (2)
-            hide        - Don't Show in Menu Bar (8)
-            always      - Always Show in Menu Bar (18)
+        Options:
+          whenActive  - Show When Active (2)
+          hide        - Don't Show in Menu Bar (8)
+          always      - Always Show in Menu Bar (18)
       '';
     };
 
     system.defaults.controlcenter.FocusModes = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "whenActive" "hide" "always" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "whenActive"
+          "hide"
+          "always"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         always = 18;
@@ -127,17 +170,24 @@ in {
         whenActive = 2;
       } 2;
       description = ''
-          Apple menu > System Preferences > Control Center > Focus
+        Apple menu > System Preferences > Control Center > Focus
 
-          Options:
-            whenActive  - Show When Active (2)
-            hide        - Don't Show in Menu Bar (8)
-            always      - Always Show in Menu Bar (18)
+        Options:
+          whenActive  - Show When Active (2)
+          hide        - Don't Show in Menu Bar (8)
+          always      - Always Show in Menu Bar (18)
       '';
     };
 
     system.defaults.controlcenter.Hearing = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 3;
@@ -146,18 +196,25 @@ in {
         hide = 12;
       } 12;
       description = ''
-          Apple menu > System Preferences > Control Center > Hearing
+        Apple menu > System Preferences > Control Center > Hearing
 
-          Options:
-            both           - Show in Menu Bar and Control Center (3)
-            menuBar        - Show in Menu Bar only (6)
-            controlCenter  - Show in Control Center only (1 or 9)
-            hide           - Don't Show (2, 4, 8, or 12)
+        Options:
+          both           - Show in Menu Bar and Control Center (3)
+          menuBar        - Show in Menu Bar only (6)
+          controlCenter  - Show in Control Center only (1 or 9)
+          hide           - Don't Show (2, 4, 8, or 12)
       '';
     };
 
     system.defaults.controlcenter.KeyboardBrightness = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 3;
@@ -166,18 +223,25 @@ in {
         hide = 12;
       } 12;
       description = ''
-          Apple menu > System Preferences > Control Center > Keyboard Brightness
+        Apple menu > System Preferences > Control Center > Keyboard Brightness
 
-          Options:
-            both           - Show in Menu Bar and Control Center (3)
-            menuBar        - Show in Menu Bar only (6)
-            controlCenter  - Show in Control Center only (1 or 9)
-            hide           - Don't Show (2, 4, 8, or 12)
+        Options:
+          both           - Show in Menu Bar and Control Center (3)
+          menuBar        - Show in Menu Bar only (6)
+          controlCenter  - Show in Control Center only (1 or 9)
+          hide           - Don't Show (2, 4, 8, or 12)
       '';
     };
 
     system.defaults.controlcenter.MusicRecognition = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 3;
@@ -186,18 +250,24 @@ in {
         hide = 12;
       } 12;
       description = ''
-          Apple menu > System Preferences > Control Center > Music Recognition
+        Apple menu > System Preferences > Control Center > Music Recognition
 
-          Options:
-            both           - Show in Menu Bar and Control Center (3)
-            menuBar        - Show in Menu Bar only (6)
-            controlCenter  - Show in Control Center only (1 or 9)
-            hide           - Don't Show (2, 4, 8, or 12)
+        Options:
+          both           - Show in Menu Bar and Control Center (3)
+          menuBar        - Show in Menu Bar only (6)
+          controlCenter  - Show in Control Center only (1 or 9)
+          hide           - Don't Show (2, 4, 8, or 12)
       '';
     };
 
     system.defaults.controlcenter.NowPlaying = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "whenActive" "hide" "always" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "whenActive"
+          "hide"
+          "always"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         always = 18;
@@ -205,17 +275,23 @@ in {
         whenActive = 2;
       } 2;
       description = ''
-          Apple menu > System Preferences > Control Center > Now Playing
+        Apple menu > System Preferences > Control Center > Now Playing
 
-          Options:
-            whenActive  - Show When Active (2)
-            hide        - Don't Show in Menu Bar (8)
-            always      - Always Show in Menu Bar (18)
+        Options:
+          whenActive  - Show When Active (2)
+          hide        - Don't Show in Menu Bar (8)
+          always      - Always Show in Menu Bar (18)
       '';
     };
 
     system.defaults.controlcenter.ScreenMirroring = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "whenActive" "hide" "always" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "whenActive"
+          "hide"
+          "always"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         always = 18;
@@ -223,17 +299,23 @@ in {
         whenActive = 2;
       } 2;
       description = ''
-          Apple menu > System Preferences > Control Center > Screen Mirroring
+        Apple menu > System Preferences > Control Center > Screen Mirroring
 
-          Options:
-            whenActive  - Show When Active (2)
-            hide        - Don't Show in Menu Bar (8)
-            always      - Always Show in Menu Bar (18)
+        Options:
+          whenActive  - Show When Active (2)
+          hide        - Don't Show in Menu Bar (8)
+          always      - Always Show in Menu Bar (18)
       '';
     };
 
     system.defaults.controlcenter.Sound = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "whenActive" "hide" "always" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "whenActive"
+          "hide"
+          "always"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         always = 18;
@@ -241,44 +323,54 @@ in {
         whenActive = 2;
       } 2;
       description = ''
-          Apple menu > System Preferences > Control Center > Sound
+        Apple menu > System Preferences > Control Center > Sound
 
-          Options:
-            whenActive  - Show When Active (2)
-            hide        - Don't Show in Menu Bar (8)
-            always      - Always Show in Menu Bar (18)
+        Options:
+          whenActive  - Show When Active (2)
+          hide        - Don't Show in Menu Bar (8)
+          always      - Always Show in Menu Bar (18)
       '';
     };
 
     system.defaults.controlcenter.Spotlight = lib.mkOption {
-        type = lib.types.nullOr lib.types.bool;
-        default = null;
-        description = ''
-          Apple menu > System Preferences > Control Center > Menu Bar Only > Spotlight
+      type = lib.types.nullOr lib.types.bool;
+      default = null;
+      description = ''
+        Apple menu > System Preferences > Control Center > Menu Bar Only > Spotlight
 
-          Show Spotlight in menu bar. Default is null.
+        Show Spotlight in menu bar. Default is null.
 
-          false - "Don't Show in Menu Bar"
-          true  - "Show in Menu Bar"
-        '';
+        false - "Don't Show in Menu Bar"
+        true  - "Show in Menu Bar"
+      '';
     };
 
     system.defaults.controlcenter.StageManager = lib.mkOption {
       type = lib.types.nullOr lib.types.bool;
       default = null;
-      apply = mkBoolApply { true = 2; false = 8; };
+      apply = mkBoolApply {
+        true = 2;
+        false = 8;
+      };
       description = ''
-          Apple menu > System Preferences > Control Center > Stage Manager
+        Apple menu > System Preferences > Control Center > Stage Manager
 
-          Show a Wi-Fi control in menu bar. Default is null.
+        Show a Wi-Fi control in menu bar. Default is null.
 
-          2 = Show in Menu Bar
-          8 = Don't Show in Menu Bar
+        2 = Show in Menu Bar
+        8 = Don't Show in Menu Bar
       '';
     };
 
     system.defaults.controlcenter.UserSwitcher = lib.mkOption {
-      type = lib.types.nullOr (lib.types.enum [ "both" "menuBar" "controlCenter" "hide" ]);
+      type = lib.types.nullOr (
+        lib.types.enum [
+          "both"
+          "menuBar"
+          "controlCenter"
+          "hide"
+        ]
+      );
       default = null;
       apply = mkEnumApply {
         both = 19;
@@ -287,27 +379,30 @@ in {
         hide = 28;
       } 28;
       description = ''
-          Apple menu > System Preferences > Control Center > Fast User Switching
+        Apple menu > System Preferences > Control Center > Fast User Switching
 
-          Options:
-            both           - Show in Menu Bar and Control Center (19)
-            menuBar        - Show in Menu Bar (22)
-            controlCenter  - Show in Control Center (25)
-            hide           - Don't Show (28)
+        Options:
+          both           - Show in Menu Bar and Control Center (19)
+          menuBar        - Show in Menu Bar (22)
+          controlCenter  - Show in Control Center (25)
+          hide           - Don't Show (28)
       '';
     };
 
     system.defaults.controlcenter.WiFi = lib.mkOption {
       type = lib.types.nullOr lib.types.bool;
       default = null;
-      apply = mkBoolApply { true = 18; false = 24; };
+      apply = mkBoolApply {
+        true = 18;
+        false = 24;
+      };
       description = ''
-          Apple menu > System Preferences > Control Center > Wi-Fi
+        Apple menu > System Preferences > Control Center > Wi-Fi
 
-          Show a Wi-Fi control in menu bar. Default is null.
+        Show a Wi-Fi control in menu bar. Default is null.
 
-          18 = Show in Menu Bar
-          24 = Don't Show in Menu Bar
+        18 = Show in Menu Bar
+        24 = Don't Show in Menu Bar
       '';
     };
 
