b221b55b210cdb11058651a51a42465b6e40be2a
angel <78835633+Iamanaws@users.noreply.github.com>
2025-02-21 13:31:35 -0800

    controlcenter: add Spotlight option

--- tests/system-defaults-write.nix
+++ tests/system-defaults-write.nix
@@ -130,6 +130,7 @@
   system.defaults.controlcenter.NowPlaying = "always";
   system.defaults.controlcenter.ScreenMirroring = "whenActive";
   system.defaults.controlcenter.Sound = "hide";
+  system.defaults.controlcenter.Spotlight = true;
   system.defaults.controlcenter.StageManager = true;
   system.defaults.controlcenter.UserSwitcher = "both";
   system.defaults.controlcenter.WiFi = true;
